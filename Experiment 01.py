21b9cfa019d17670a7bc9f0848479e38d25e1571
Nirmal <thariyannirmaljohnkallukaran@gmail.com>
2026-09-16 21:03:41 +0530

    Solved some Lab Questiones

---
+++ Experiment 01.py
@@ -0,0 +1,34 @@
+#Develop a Python program to solve basic computational problems by accepting user input and performing arithmetic
+#and logical operations such as finding the area of a circle, checking leap years, and determining the largest of three numbers.
+
+
+#Area of a circle
+import math
+r = float(input("Enter the radius of the circle: "))
+area = math.pi*r*r
+print("The area of the circle is:", area)
+
+#Leap year checker
+year = int(input("Enter a year : "))
+if (year % 400 == 0):
+    print(year, "is a leap year")
+elif (year % 100 == 0):
+    print(year, "is not a leap year")
+elif (year % 4 == 0):
+    print(year, "is a leap year")
+else:
+    print(year, "is not a leap year")
+
+#Finding the largest of three numbers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+if (a >= b) and (a >= c):
+    largest = a
+elif (b >= a) and (b >= c):
+    largest = b
+else:
+    largest = c
+
+print("The largest number is:", largest)
